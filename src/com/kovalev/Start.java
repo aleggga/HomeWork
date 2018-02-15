2f0970627ebe866fcbeca69378e2001c0db3ead0
aleggga <35814093+aleggga@users.noreply.github.com>
2018-02-15 19:18:32 +0200

    Update Start.java

--- src/com/kovalev/Start.java
+++ src/com/kovalev/Start.java
@@ -25,7 +25,7 @@ public class Start {
         System.out.println(Calculator.avg(10.54,3));
         System.out.println(Calculator.pow(6));
         System.out.println(Calculator.pow(10.54));
-        System.out.println(Calculator.percent(100, 10));*/
+        System.out.println(Calculator.percent(100, 10));
 
                     /// HOMEWORK 2 ///
             
@@ -39,6 +39,7 @@ public class Start {
         List<Integer> dates = new ArrayList<Integer>(Arrays.asList(1910, 1700, 1998, 2000, 2001, 200, 381, 45, 1000, 1001));
         for (int a : dates) {
             System.out.println(DatesTool.dateToEpoch(a));
+        }
       
                     /// HOMEWORK 4 ///
             
