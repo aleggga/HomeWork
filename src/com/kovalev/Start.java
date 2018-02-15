88485f6c4da8a2617a0f58582ce17d7e7513d4c7
aleggga <35814093+aleggga@users.noreply.github.com>
2018-02-15 19:14:55 +0200

    Update Start.java

--- src/com/kovalev/Start.java
+++ src/com/kovalev/Start.java
@@ -8,10 +8,9 @@ public class Start {
 
     public static void main(String[] args) {
 
-/*/// HOMEWORK 1 ///
-*//*
-Реализовать класс MyCalc для вещественных чисел (чисел с плавающей точкой) со следующим набором методов/функций:
-*//*
+                /// HOMEWORK 1 ///
+        
+//Реализовать класс MyCalc для вещественных чисел (чисел с плавающей точкой) со следующим набором методов/функций:
         System.out.println(Calculator.add(1,3));
         System.out.println(Calculator.add(1.1,3.2));
         System.out.println(Calculator.sub(4,3));
@@ -28,23 +27,21 @@ public class Start {
         System.out.println(Calculator.pow(10.54));
         System.out.println(Calculator.percent(100, 10));*/
 
-/*
-/// HOMEWORK 2 ///
+                    /// HOMEWORK 2 ///
+            
         System.out.println(Calculator.castToLong(100.134));
-*/
 
-
-/*/// HOMEWORK 3 ///
-*//*
-Написать функцию определения столетия по номеру года.
-Диапазон возможных значений для года может лежать в пределах от 1 до 2018.
-*//*
+        
+                    /// HOMEWORK 3 ///
+        
+//Написать функцию определения столетия по номеру года.
+//Диапазон возможных значений для года может лежать в пределах от 1 до 2018.
         List<Integer> dates = new ArrayList<Integer>(Arrays.asList(1910, 1700, 1998, 2000, 2001, 200, 381, 45, 1000, 1001));
         for (int a : dates) {
             System.out.println(DatesTool.dateToEpoch(a));
-        }*/
-
-        /// HOMEWORK 4 ///
+      
+                    /// HOMEWORK 4 ///
+            
 //a. Написать два варианта ф-ции определения сезона года по номеру месяца.
 //Первый вариант ф-ции должен использовать if .. else .. if оператор.
 //Второй вариант ф-ции должен использовать switch .. case.
@@ -75,7 +72,7 @@ public class Start {
 //Если "0.0", то немедленно выйти из цикла.
 
 
-//        EagleTails.getTailsOrEagles();
+//      EagleTails.getTailsOrEagles();
 
 //d. Организовать циклы do while обратного отсчета от 10 до 1 в выводом на экран.
 
@@ -101,60 +98,41 @@ public class Start {
 
         Calculator.getIncrementedValues(10, 38, 2);
 
-/// HomeWork 5 ///
-/*
-
-Определить является ли число палиндромом
-
-*/
+                    /// HomeWork 5 ///
+            
+//Определить является ли число палиндромом
         System.out.println(Calculator.ifNumberPalendrom(123456)); // NO
         System.out.println(Calculator.ifNumberPalendrom(123321)); // YES
         System.out.println(Calculator.ifNumberPalendrom(9239329)); // YES
 
-/*
-
-Двумерный массив 10х10, заполнить случайными числами от -20 до 20.
-
-*/
+//Двумерный массив 10х10, заполнить случайными числами от -20 до 20.
         int[][] arr2 = ArrayTools.getArrayWithRandomValues(10,10,-20,20);
-/*
-определения суммы всех элементов массива,
-*/
+
+//определения суммы всех элементов массива,
                 System.out.println(ArrayTools.getSummOfAllArrayElements(arr2));
-/*
-среднее арифметическое,
-*/
+
+//среднее арифметическое,
         System.out.println(ArrayTools.getArr2ElementsAverage(arr2));
-/*
-минимальный элемент,
-*/
+
+//минимальный элемент
         System.out.println(ArrayTools.getArr2ElementsMin(arr2));
 
-/*максималный элемент,*/
+//максималный элемент,
         System.out.println(ArrayTools.getArr2ElementsMax(arr2));
-/*
-кол-во положительных элементов,
-кол-во отрицательных элементов.
-*/
+            
+//кол-во положительных элементов,
+//кол-во отрицательных элементов.
         System.out.println(ArrayTools.getArr2NumOfPositiveElements(arr2, false));
 
-/*
-c. В числовой матрице 5x2 поменять местами два столбца,
-т. е. все элементы одного столбца поставить на соответствующие им позиции другого,
-а его элементы второго переместить в первый.
-*/
+//c. В числовой матрице 5x2 поменять местами два столбца,
+//т. е. все элементы одного столбца поставить на соответствующие им позиции другого,
+//а его элементы второго переместить в первый.
         int[][] arr3 = ArrayTools.getArrayWithRandomValues(5,2,10,20);
         arr3 = ArrayTools.switchArr2columnsElements(arr3, 0,1);
 
-/*
-
-d. Найти столбец матрицы с максимальной суммой элементов:
-Задана матрица неотрицательных чисел 3x5. Посчитать сумму элементов в каждом столбце. Определить, какой столбец содержит максимальную сумму.
-
-*/
+//d. Найти столбец матрицы с максимальной суммой элементов:
+//Задана матрица неотрицательных чисел 3x5. Посчитать сумму элементов в каждом столбце. Определить, какой столбец содержит максимальную сумму.
         int[][] arr4 = ArrayTools.getArrayWithRandomValues(3,5,10,20);
         System.out.println("Max column total value: " + ArrayTools.getMaxArray2Column(arr4));
     }
-
-
 }
